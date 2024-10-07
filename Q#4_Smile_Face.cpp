bab02a3df159aab288d076fdade55cdd60c66a16
Hassan <hassanjaan731@gmail.com>
2024-10-07 10:35:45 +0500

    Add Q#4 Assignment_no_3

---
+++ Q#4_Smile_Face.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int  main()
+{
+	char smile_1 = 58;
+	char smile_2 = 41;
+	for (int i = 1; i <= 768; i++)
+	{
+		for (int i = 1; i <= 1366; i++)
+		{
+			cout << smile_1 << smile_2 ;
+		}
+
+	}
+
+}
