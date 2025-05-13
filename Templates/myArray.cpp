f7aa0e9b6c86a49180716c24ebd349f4e8a4a6a4
Hassan <hassanjaan731@gmail.com>
2025-05-13 10:13:45 +0500

    Modify myArray.cpp

--- Templates/myArray.cpp
+++ Templates/myArray.cpp
@@ -19,11 +19,11 @@ template <class T>
 myArray<T> :: myArray(const myArray& orig)
 {
   this->arraySize = orig.arraySize;
-  this->aPtr = new int[orig.arraySize]; 
+  this->aPtr = new T[orig.arraySize]; 
   for (int i = 0; i < arraySize; i++)
   {
     this->aPtr[i] = orig.aPtr[i];
-  }
+  }s
 }
 
 template <class T>
