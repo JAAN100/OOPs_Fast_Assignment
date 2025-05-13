5d7025c499a47b3df375a4035c208a2e8edbd681
Hassan <hassanjaan731@gmail.com>
2025-05-13 10:09:47 +0500

    Modify myArray.cpp

--- Templates/myArray.cpp
+++ Templates/myArray.cpp
@@ -1,38 +1,47 @@
-#include <istream>
+#include <iostream>
 #include "myArray.h"
-using namespace std; 
-// void myArray :: subScriptError()
-// {
-//     cout << "Error invalid input" <<endl;
-//     exit(0);
-// }
-// myArray :: myArray(int a)
-// {
-//     arraySize = a;
-//     aPtr = new T [arraySize];
-// }
-// myArray :: myArray(const myArray& orig)
-// {
-//   this->arraySize = orig.arraySize;
-//   this->aPtr = new int[orig.arraySize]; 
-//   for (int i = 0; i < arraySize; i++)
-//   {
-//     this->aPtr[i] = orig.aPtr[i];
-//   }
-// }
-// myArray :: ~myArray()
-// {
-//     if (aPtr != nullptr)
-//     {
-//         delete [] aPtr;
-//         aPtr = nullptr;
-//     }
-// }
-// int & myArray :: operator[](const int& rhs)
-// {
-//     if (rhs < 0 || rhs >= arraySize)
-//     {
-//         subScriptError();
-//     }   
-//    return aPtr[rhs];   
-// }
+using namespace std;
+template <class T>
+void myArray<T> :: subScriptError()
+{
+    cout << "Error invalid input" <<endl;
+    exit(0);
+}
+
+template <class T>
+myArray<T> :: myArray(int a)
+{
+    arraySize = a;
+    aPtr = new T [arraySize];
+}
+
+template <class T>
+myArray<T> :: myArray(const myArray& orig)
+{
+  this->arraySize = orig.arraySize;
+  this->aPtr = new int[orig.arraySize]; 
+  for (int i = 0; i < arraySize; i++)
+  {
+    this->aPtr[i] = orig.aPtr[i];
+  }
+}
+
+template <class T>
+myArray<T> :: ~myArray()
+{
+    if (aPtr != nullptr)
+    {
+        delete [] aPtr;
+        aPtr = nullptr;
+    }
+}
+
+template <class T>
+T & myArray<T>:: operator[](const int& rhs)
+{
+    if (rhs < 0 || rhs >= arraySize)
+    {
+        subScriptError();
+    }   
+   return aPtr[rhs];   
+}
